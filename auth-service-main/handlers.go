181032fa835411d350362905d53b9bcb666e6823
Jefferson <jffreitas.poli@gmail.com>
2026-09-15 20:52:31 -0300

    teste: correção

--- auth-service-main/handlers.go
+++ auth-service-main/handlers.go
@@ -24,7 +24,11 @@ type CreateKeyResponse struct {
 // healthHandler é um simples endpoint de verificação de saúde
 func (a *App) healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	if err := json.NewEncoder(w).Encode(map[string]string{"status": "ok"}); err != nil {
+		log.Printf("Falha no json encode da mensagem de retorno")
+		http.Error(w, "Falha na criação da mensagem de retorno do healthcheck", http.StatusUnauthorized)
+		return
+	}
 }
 
 // validateKeyHandler verifica se uma chave de API (enviada via Header) é válida
